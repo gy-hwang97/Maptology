6932b886c5ee1fc17fd90787fc1c80f440c116ea
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2025-07-23 18:06:32 -0600

    Update components.py

--- components.py
+++ components.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-# 로고와 제목을 컬럼으로 표시
+# 로고와 제목을 컬럼으로 표시 / Display logo and title in columns
 def render_header():
     col1, col2 = st.columns([1, 10])
     with col1:
-        st.image("maptology.png", width=200)  # 경로 슬래시 방향 수정
+        st.image("maptology.png", width=200)  # 경로 슬래시 방향 수정 / Fixed path slash direction
     with col2:
         st.title('Maptology')
